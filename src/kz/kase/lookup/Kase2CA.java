baeda84e31cb5082c0da96a2356359fe84ca2027
marcgereski <marc.gereski@gmail.com>
2016-03-17 22:43:37 +0600

    created main procedure

--- src/kz/kase/lookup/Kase2CA.java
+++ src/kz/kase/lookup/Kase2CA.java
@@ -1,7 +1,7 @@
 package kz.kase.lookup;
 
 
-import kz.kase.lookup.domain.DepolFile;
+import kz.kase.lookup.domain.DepoFile;
 import kz.kase.lookup.util.LinuxCommand;
 
 import java.io.IOException;
@@ -10,39 +10,83 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Kase2CA {
-    public List<DepolFile> getDepoFiles(String directory) throws IOException {
+    public List<DepoFile> getDepoFiles(String directory) throws IOException {
         String filesList = LinuxCommand
                 .run("ls -l --time-style=+'%Y-%m-%d %H:%M:%S' " + directory
                         + "| grep -i '' | cut -d ' ' -f8-10", "\n");
         String[] fs = filesList.split("\n");
         List<String> fls = Arrays.asList(new String[fs.length]);
 
-        List<DepolFile> depolFiles = new ArrayList<>();
-        String[] fline = {};
+        List<DepoFile> depoFiles = new ArrayList<>();
+        String[] fline;
         for (String f : fls) {
             fline = f.split(" ");
-            depolFiles.add(new DepolFile(fline[2], fline[0], fline[1]));
+            depoFiles.add(new DepoFile(fline[2], fline[0], fline[1]));
         }
-        return depolFiles;
+        return depoFiles;
+    }
+
+    public String getFileModificationTime(String directory, String filename) throws IOException {
+        return LinuxCommand
+                .run("ls -l --time-style=+'%Y-%m-%d %H:%M:%S' " + directory
+                        + "| grep -i '" + filename + "' | cut -d ' ' -f8,9", "");
     }
 
     public String getFileContent(String file, String pattern) throws IOException {
         return LinuxCommand.run("grep -i '" + pattern + "' " + file, "");
     }
 
-    public void checkEtransferOutDir(String directory, DepolFile depolFile) throws IOException {
-        String f = LinuxCommand.run("ls -l --time-style=+'%Y-%m-%d %H:%M:%S' " + directory
-                + "| grep -i '" + depolFile + "' | cut -d ' ' -f8-10", "");
-        if (!f.isEmpty()) depolFile.setIsSentAccordingToEtrDir(true);
-        else depolFile.setIsSentAccordingToEtrDir(false);
+    public String getFilenameByPattern(String file, String pattern) throws IOException {
+        return LinuxCommand.run("grep -i '" + pattern + "' " + file, "");
     }
 
-    public void checkConverterData(String converterFile, DepolFile depolFile) throws IOException {
-        String res = LinuxCommand.run("grep -i -A3 '" + depolFile.getFilename() + "' " +
-                converterFile + " | grep -i 'status'", "");
-        boolean status = false;
-        if (res.contains(DepolFile.CONVERTED)) status = true;
+    public void checkEtransferOutDir(String directory, DepoFile depoFile) throws IOException {
+        String f = LinuxCommand.run("ls -l --time-style=+'%Y-%m-%d %H:%M:%S' " + directory
+                + "| grep -i '" + depoFile + "' | cut -d ' ' -f8-10", "");
+        if (!f.isEmpty()) depoFile.setIsSentAccordingToEtrDir(true);
+        else depoFile.setIsSentAccordingToEtrDir(false);
+    }
 
-        depolFile.setIsConverted(status);
+    public boolean checkConverterData(String converterFile, DepoFile depoFile) {
+        try {
+            String res = LinuxCommand.run("grep -i -A3 '" + depoFile.getFilename() + "' " +
+                    converterFile + " | grep -i 'status'", "");
+            if (res.contains(DepoFile.CONVERTED)) return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        final String depoDir = "";
+        final String converterFile = "";
+        final String etrOutDir = "";
+        final String etrInDir = "";
+
+        Kase2CA app = new Kase2CA();
+
+        List<DepoFile> depoFileList = app.getDepoFiles("");
+
+        for (DepoFile file : depoFileList) {
+            file.setCodeTo(app.getFileContent(depoDir + "/" + file.getFilename(), ":20:"));
+            file.setDealId(app.getFileContent(depoDir + "/" + file.getFilename(), ":DL_:"));
+            file.setRefNum(app.getFileContent(depoDir + "/" + file.getFilename(), ":REF:"));
+            file.setIsConverted(app.checkConverterData(converterFile, file));
+            String sentTimeFilesystem = app.getFileModificationTime(etrOutDir, file.getFilename());
+            if (sentTimeFilesystem.isEmpty()) {
+                file.setIsSentAccordingToEtrDir(false);
+            } else {
+                file.setIsSentAccordingToEtrDir(true);
+                file.setSentTimeAccordingToEtrDir(sentTimeFilesystem);
+            }
+            file.setResponseFile(app.getFilenameByPattern(etrInDir + "/" + file.getFilename(), ":21:" + file.getCodeTo()));
+            file.setResponseTime(app.getFileModificationTime(etrInDir, file.getFilename()));
+
+            System.out.println(file.toString());
+            System.out.println("---------------------------------------------------------");
+        }
+
     }
 }
---
+++ src/kz/kase/lookup/domain/DepoFile.java
@@ -0,0 +1,128 @@
+package kz.kase.lookup.domain;
+
+import java.util.Date;
+
+/**
+ * Created by MM on 17.03.2016.
+ */
+public class DepoFile {
+    //    private static final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static final String CONVERTED = "Converted";
+
+    private final String filename;
+    private final String date;
+    private final String time;
+    private final Date datetime = null;
+
+    private String dealId;
+    private String refNum;
+    private String codeTo;
+    private boolean isConverted;
+    private boolean isSentAccordingToEtrDir;
+    private String sentTimeAccordingToEtrDir;
+    private String responseFile = null;
+    private String responseTime = null;
+
+    public DepoFile(String filename, String date, String time) {
+        this.filename = filename;
+        this.date = date;
+        this.time = time;
+//        datetime = df.parse(date+" "+time);
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public boolean isConverted() {
+        return isConverted;
+    }
+
+    public boolean isSentAccordingToEtrDir() {
+        return isSentAccordingToEtrDir;
+    }
+
+    public String getResponseFile() {
+        return responseFile;
+    }
+
+    public String getDealId() {
+        return dealId;
+    }
+
+    public String getRefNum() {
+        return refNum;
+    }
+
+    public String getResponseTime() {
+        return responseTime;
+    }
+
+    public String getCodeTo() {
+        return codeTo;
+    }
+
+    public String getSentTimeAccordingToEtrDir() {
+        return sentTimeAccordingToEtrDir;
+    }
+
+    public void setDealId(String dealId) {
+        this.dealId = dealId;
+    }
+
+    public void setRefNum(String refNum) {
+        this.refNum = refNum;
+    }
+
+    public void setCodeTo(String codeTo) {
+        this.codeTo = codeTo;
+    }
+
+    public void setIsConverted(boolean isConverted) {
+        this.isConverted = isConverted;
+    }
+
+    public void setIsSentAccordingToEtrDir(boolean isInEtrDir) {
+        this.isSentAccordingToEtrDir = isInEtrDir;
+    }
+
+    public void setResponseFile(String responseFile) {
+        this.responseFile = responseFile;
+    }
+
+    public void setSentTimeAccordingToEtrDir(String sentTimeAccordingToEtrDir) {
+        this.sentTimeAccordingToEtrDir = sentTimeAccordingToEtrDir;
+    }
+
+    public void setResponseTime(String responseTime) {
+        this.responseTime = responseTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Filename:  ").append(filename).append("\n");
+        sb.append("Created:   ").append(date).append(" ").append(time).append("\n");
+        sb.append("Deal ID:   ").append(dealId).append("\n");
+        sb.append("RefNum:    ").append(refNum).append("\n");
+        sb.append("Code:      ").append(codeTo).append("\n");
+        sb.append("Converted: ").append(isConverted).append("\n");
+        sb.append("Found in Out Dir:   ").append(isSentAccordingToEtrDir).append("\n");
+        sb.append("Created in Out Dir: ").append(sentTimeAccordingToEtrDir).append("\n");
+        sb.append("Response file: ").append(responseFile).append("\n");
+        sb.append("Response Time: ").append(responseTime).append("\n");
+        return sb.toString();
+    }
+}
